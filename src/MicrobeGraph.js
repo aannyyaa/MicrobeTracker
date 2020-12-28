7fd2f43aaecb7f2c83e542d2e7185364bc59f630
Anya Dunaif <anya.dunaif@gmail.com>
2020-12-28 12:01:53 -0500

    working on bar chart formatting

--- src/MicrobeGraph.js
+++ src/MicrobeGraph.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { VictoryBar, VictoryChart, VictoryAxis} from 'victory';
 
 const data = [
-  { bacteria: 'bacteria1', number: 10 },
-  { bacteria: 'bacteria2', number: 5 },
-  { bacteria: 'bacteria3', number: 7 },
-  { bacteria: 'bacteria4', number: 2 },
-  { bacteria: 'bacteria5', number: 4 },
+  { bacteria: '1', relativeFreq: 10 },
+  { bacteria: '2', relativeFreq: 5 },
+  { bacteria: '3', relativeFreq: 7 },
+  { bacteria: '4', relativeFreq: 2 },
+  { bacteria: '5', relativeFreq: 4 },
 ];
 
 class MicrobeGraph extends React.Component {
   render() {
     return (
       <VictoryChart domainPadding={25}>
-        <VictoryBar data={data} x="bacteria" y="number" />
+        <VictoryAxis
+          tickValues={[1,2,3,4,5,6,7,8,9,10,11,12]}
+          tickFormat={(x)=>(`Sample ${x}`)}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x)=>(`${x}%`)}
+        />
+        <VictoryBar data={data} x="bacteria" y="relativeFreq" />
       </VictoryChart>
     );
   }
