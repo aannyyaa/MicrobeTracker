ca98933e9af5c2f0770d0014da5a067aa9f61c01
Anya Dunaif <anya.dunaif@gmail.com>
2020-12-28 12:35:27 -0500

    added dummy data for frequency

--- src/MicrobeGraph.js
+++ src/MicrobeGraph.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { VictoryBar, VictoryChart, VictoryAxis} from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryStack } from 'victory';
 
-const data = [
-  { bacteria: '1', relativeFreq: 10 },
-  { bacteria: '2', relativeFreq: 5 },
-  { bacteria: '3', relativeFreq: 7 },
-  { bacteria: '4', relativeFreq: 2 },
-  { bacteria: '5', relativeFreq: 4 },
+const microbe1 = [
+  { sample: '1', relativeFreq: 55 },
+  { sample: '2', relativeFreq: 5 },
+  { sample: '3', relativeFreq: 10 },
+  { sample: '4', relativeFreq: 15 },
+  { sample: '5', relativeFreq: 65 },
+];
+const microbe2 = [
+  { sample: '1', relativeFreq: 5 },
+  { sample: '2', relativeFreq: 45 },
+  { sample: '3', relativeFreq: 20 },
+  { sample: '4', relativeFreq: 10 },
+  { sample: '5', relativeFreq: 10 },
+];
+const microbe3 = [
+  { sample: '1', relativeFreq: 15 },
+  { sample: '2', relativeFreq: 15 },
+  { sample: '3', relativeFreq: 25 },
+  { sample: '4', relativeFreq: 20 },
+  { sample: '5', relativeFreq: 5 },
+];
+const microbe4 = [
+  { sample: '1', relativeFreq: 20 },
+  { sample: '2', relativeFreq: 5 },
+  { sample: '3', relativeFreq: 5 },
+  { sample: '4', relativeFreq: 35 },
+  { sample: '5', relativeFreq: 10 },
+];
+const microbe5 = [
+  { sample: '1', relativeFreq: 5 },
+  { sample: '2', relativeFreq: 30 },
+  { sample: '3', relativeFreq: 40 },
+  { sample: '4', relativeFreq: 20 },
+  { sample: '5', relativeFreq: 10 },
 ];
 
 class MicrobeGraph extends React.Component {
@@ -14,14 +42,17 @@ class MicrobeGraph extends React.Component {
     return (
       <VictoryChart domainPadding={25}>
         <VictoryAxis
-          tickValues={[1,2,3,4,5,6,7,8,9,10,11,12]}
-          tickFormat={(x)=>(`Sample ${x}`)}
+          tickValues={[1, 2, 3, 4, 5]}
+          tickFormat={(x) => `Sample ${x}`}
         />
-        <VictoryAxis
-          dependentAxis
-          tickFormat={(x)=>(`${x}%`)}
-        />
-        <VictoryBar data={data} x="bacteria" y="relativeFreq" />
+        <VictoryAxis dependentAxis tickFormat={(x) => `${x}%`} />
+        <VictoryStack>
+          <VictoryBar data={microbe1} x="sample" y="relativeFreq" />
+          <VictoryBar data={microbe2} x="sample" y="relativeFreq" />
+          <VictoryBar data={microbe3} x="sample" y="relativeFreq" />
+          <VictoryBar data={microbe4} x="sample" y="relativeFreq" />
+          <VictoryBar data={microbe5} x="sample" y="relativeFreq" />
+        </VictoryStack>
       </VictoryChart>
     );
   }
