be6ca67208a710bf1636fbb4ca14d0cb0b4bf4b5
Anya Dunaif <anya.dunaif@gmail.com>
2021-01-03 21:29:38 -0500

    switched styled menu back to simple menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,39 +1,7 @@
 import React from 'react';
-import { Button, Menu, MenuItem, ListItemIcon} from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { Button, Menu, MenuItem, ListItemIcon } from '@material-ui/core';
 import { Home, Map, Room, LocationCity } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
-
-const StyledMenu = withStyles({
-  paper: {
-    border: '1px solid #000000',
-  },
-})((props) => (
-  <Menu
-    elevation={0}
-    git
-    getContentAnchorEl={null}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'top',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
-    }}
-    {...props}
-  />
-));
-
-const StyledMenuItem = withStyles((theme) => ({
-  root: {
-    '&:focus': {
-      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-        color: theme.palette.common.white,
-      },
-    },
-  },
-}))(MenuItem);
 
 export default function NavMenu() {
   const [anchor, setAnchor] = React.useState(null);
@@ -47,55 +15,54 @@ export default function NavMenu() {
   };
 
   return (
-    <div id="navMenu">
+    <div>
       <Button
-        aria-controls="customized-menu"
+        aria-controls="simple-menu"
         aria-haspopup="true"
-        variant="contained"
         onClick={handleClick}
       >
         Menu
       </Button>
-      <StyledMenu
-        id="customized-menu"
+      <Menu
+        id="simple-menu"
         anchorEl={anchor}
         keepMounted
         open={Boolean(anchor)}
         onClose={handleClose}
       >
-        <StyledMenuItem onClick={handleClose}>
+        <MenuItem onClick={handleClose}>
           <ListItemIcon>
             <Home />
           </ListItemIcon>
           <Link to="/">
             <p>Home</p>
           </Link>
-        </StyledMenuItem>
-        <StyledMenuItem onClick={handleClose}>
-        <ListItemIcon>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
             <Map />
           </ListItemIcon>
           <Link to="/map">
             <p>Map</p>
           </Link>
-        </StyledMenuItem>
-        <StyledMenuItem onClick={handleClose}>
-        <ListItemIcon>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
             <Room />
           </ListItemIcon>
           <Link to="/graph-location">
-            <p>Track a Location</p>
+            <p>Graph by Location</p>
           </Link>
-        </StyledMenuItem>
-        <StyledMenuItem onClick={handleClose}>
-        <ListItemIcon>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
             <LocationCity />
           </ListItemIcon>
           <Link to="/graph-area">
-            <p>Track a Borough</p>
+            <p>Graph for an Area</p>
           </Link>
-        </StyledMenuItem>
-      </StyledMenu>
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
